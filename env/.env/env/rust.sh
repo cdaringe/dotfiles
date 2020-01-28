af4ebf4d772f5e36acea3f7273ef118d6e370c5d
cdaringe <cdaringe@gmail.com>
2020-01-27 19:55:17 -0800

    feat(dotfiles): enhancing the radness

--- env/.env/env/rust.sh
+++ env/.env/env/rust.sh
@@ -2,5 +2,5 @@
 export PATH="$HOME/.cargo/bin:$PATH"
 if [ -f "$HOME/.cargo/env" ]; then
   . $HOME/.cargo/env
-  export RUST_SRC_PATH=~/.multirust/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src/
+  export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
 fi
