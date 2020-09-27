cdd96340f93a01ca439624b2b8d3503b20dff57e
cdaringe <christopher.dieringer@gmail.com>
2020-09-26 17:55:30 -0700

    feat(dotfiles): enhancing the radness

---
+++ env/.basher.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# setup startup profiling
+# ts = timestamp
+alias ts="true"
+ts;
+alias tsflush="true"
+alias tslog=""
+if hash gdate 2>/dev/null
+then
+  true
+  # enable for debug mode
+  # comment out-for noop!
+  # unalias ts
+  # unalias tsflush
+  # function ts() {
+  #   tslog="$tslog$(gdate +%s%N),$1\n"
+  # }
+  # function tsflush() {
+  #   printf "timestamp,call\n$tslog" > ~/.startup.debug.csv
+  # }
+fi
+
+[ -z "$PS1" ] && return
+export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
+
+# bash specific aliases
+if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases; fi
+
+# load all env, apps, & aliases files
+for file in ~/.env/env/*.sh; do
+  ts "load_env_$file"
+  . "$file"
+done
+for file in ~/.env/aliases/*.sh; do
+  ts "load_aliases_$file"
+  . "$file";
+done
+for file in ~/.env/apps/*.sh; do
+  ts "load_apps_$file"
+  . "$file";
+done
+
+# load untracked files ;)
+ts "load_secrets"
+if [ -f ~/.secrets.sh ]; then . ~/.secrets.sh; fi
+ts "load_work"
+if [ -f ~/.work.sh ]; then . ~/.work.sh; fi
+
+ts "load_/etc/bashrc"
+if [ -f /etc/bashrc ]; then . /etc/bashrc; fi
+ts "done"
+tsflush
+
+echo 🌲
--- env/.env/env/node.sh
+++ env/.env/env/node.sh
@@ -1,34 +1,12 @@
 #!/bin/bash
 export NODE_ENV='development'
-export NVM_DIR="$HOME/.nvm"
-export IS_NVM_LOADED=;
 
-function load_nvm () {
-  echo "lazy loading nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-}
-
-function _load_nvm () {
-  unalias nvm;
-  unalias node;
-  load_nvm;
-}
-
-function nvm_lazy () {
-  _load_nvm;
-  nvm "$@";
-}
-function node_lazy () {
-  _load_nvm;
-  node "$@"
-}
-alias nvm="nvm_lazy"
-alias node="node_lazy"
+export PATH=/home/cdaringe/.fnm:$PATH
+eval "`fnm env --multi`"
 
 load-nvmrc() {
   if [[ -f .nvmrc && -r .nvmrc ]]; then
-    nvm use
+    fnm use
   fi
   # if [[ -f .tool-versions && -r .tool-versions ]]; then
     # asdf local $(cat .tool-versions)
