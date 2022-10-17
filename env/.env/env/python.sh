4a0f8c9416435ae53ed1c1a7001a2064d27452b3
c0d01a5 <christopher.dieringer@walmart.com>
2022-10-17 14:19:54 -0700

    feat(dotfiles): enhancing the radness

--- env/.env/env/python.sh
+++ env/.env/env/python.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 export PIPENV_VENV_IN_PROJECT=1
 export PYTHON_DEV=1
+if ! command_exists python; then
+  if command_exists python3; then
+    # export PATH="$(which python3):$PATH"
+    alias python=python3
+  fi
+fi
