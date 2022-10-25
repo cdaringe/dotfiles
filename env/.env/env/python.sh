9b7e250b3dda78e9ecb7dbf80946e710efd5e962
c0d01a5 <christopher.dieringer@walmart.com>
2022-10-25 15:43:51 -0700

    feat(dotfiles): enhancing the radness

--- env/.env/env/python.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PIPENV_VENV_IN_PROJECT=1
-export PYTHON_DEV=1
-if ! command_exists python; then
-  if command_exists python3; then
-    echo "linking python3 as python to /usr/local/bin"
-    ln -s "$(which python3)" /usr/local/bin/python
-  fi
-fi
