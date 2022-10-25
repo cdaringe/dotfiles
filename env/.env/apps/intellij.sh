ec6b11f878208fce0428891d9302dad3c34e7d37
c0d01a5 <christopher.dieringer@walmart.com>
2022-10-25 15:43:25 -0700

    feat(dotfiles): enhancing the radness

---
+++ env/.env/apps/intellij.sh
@@ -0,0 +1,5 @@
+#!/usr/env/bin bash
+
+function idea() {
+  open -na "IntelliJ IDEA.app" --args "$@"
+}
--- env/.env/env/python.sh
+++ env/.env/env/python.sh
@@ -3,7 +3,7 @@ export PIPENV_VENV_IN_PROJECT=1
 export PYTHON_DEV=1
 if ! command_exists python; then
   if command_exists python3; then
-    # export PATH="$(which python3):$PATH"
-    alias python=python3
+    echo "linking python3 as python to /usr/local/bin"
+    ln -s "$(which python3)" /usr/local/bin/python
   fi
 fi
