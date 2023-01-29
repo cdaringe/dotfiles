f878f2721f9f74e070b4bfcf9fc924323177fbdc
cdaringe <cdaringe@gmail.com>
2023-01-28 16:25:27 -0800

    feat(dotfiles): enhancing the radness

--- env/.env/apps_pre.sh
+++ env/.env/apps_pre.sh
@@ -3,7 +3,8 @@
 # Get brew executables on PATH immediately
 export PATH="/opt/homebrew/bin/:$PATH"
 
-brew_to_install=("hub" "tree" "jq" "ffmpeg")
+# See apps_post.js for syntax
+brew_to_install=("bash" "hub" "tree" "jq" "ffmpeg" "jtd-codegen::jsontypedef/jsontypedef/jtd-codegen")
 brew_to_install_cask=("caffeine" "flux" "flycut" "spectacle" "iterm2" "docker" "visual-studio-code" "evernote" "wireshark" "bloomrpc")
 function add_brew_install() {
   brew_to_install+=("$1")
