86239b68ee82eae110134557316f9c864b16161b
cdaringe <cdaringe@gmail.com>
2023-02-05 16:42:09 -0800

    feat(dotfiles): enhancing the radness

--- env/.env/apps/portland_clean_air.sh
+++ env/.env/apps/portland_clean_air.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-# devtime only secret :) nice try suckas
+# devtime only secret :) nice try suckas.
+# see secrets.sh (untracked) for alt env values
 export PCA_GQL_ADMIN_SECRET=airman
 export HASURA_GRAPHQL_ADMIN_SECRET="$PCA_GQL_ADMIN_SECRET"
