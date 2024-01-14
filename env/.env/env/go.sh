ea3cc1a45aada6deb897446f715f9b3ae45032ed
cdaringe <christopher.dieringer@gmail.com>
2024-01-13 16:28:49 -0800

    feat(dotfiles): enhancing the radness

--- env/.env/env/go.sh
+++ env/.env/env/go.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if ! command_exists go; then
-  export GOPATH=$HOME/go
-  export GOROOT="$(/opt/homebrew/opt/go)"
-  export PATH=$PATH:$GOPATH/bin
+  if [ $IS_DARWIN ]; then
+    export GOPATH=$HOME/go
+    export GOROOT="$(/opt/homebrew/opt/go)"
+    export PATH=$PATH:$GOPATH/bin
+  fi
 fi
