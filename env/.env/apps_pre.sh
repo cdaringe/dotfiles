43b6c6935115e363f96860c8bcfead5f5f58c7f7
cdaringe <cdaringe@gmail.com>
2024-01-27 10:50:27 -0800

    feat(dotfiles): enhancing the radness

--- env/.env/apps_pre.sh
+++ env/.env/apps_pre.sh
@@ -4,7 +4,7 @@
 export PATH="/opt/homebrew/bin/:$PATH"
 
 # See apps_post.js for syntax
-brew_to_install=("bash" "dot::graphviz" "hub" "tree" "nmap" "jq" "ffmpeg" "jtd-codegen::jsontypedef/jsontypedef/jtd-codegen" "sonar-scanner")
+brew_to_install=("bash" "dot::graphviz" "hub" "tree" "nmap" "jq" "mosh" "ffmpeg" "jtd-codegen::jsontypedef/jsontypedef/jtd-codegen" "sonar-scanner")
 brew_to_install_cask=("caffeine" "obs" "flux" "flycut" "github" "rectangle" "iterm2" "visual-studio-code" "wireshark" "bloomrpc" "podman-desktop")
 function add_brew_install() {
   brew_to_install+=("$1")
