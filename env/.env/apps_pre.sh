cf025d344197c884f8a02b08693766549a936ce4
cdaringe <cdaringe@gmail.com>
2024-05-15 10:49:11 -0700

    feat(dotfiles): enhancing the radness

--- env/.env/apps_pre.sh
+++ env/.env/apps_pre.sh
@@ -5,7 +5,7 @@ export PATH="/opt/homebrew/bin/:$PATH"
 
 # See apps_post.js for syntax
 brew_to_install=("bash" "fd" "hub" "tree" "jq" "mosh" "ffmpeg")
-brew_to_install_cask=("caffeine" "obs" "flux" "flycut" "rectangle" "iterm2" "visual-studio-code" "wireshark" "podman-desktop")
+brew_to_install_cask=("obs" "flux" "flycut" "rectangle" "iterm2" "visual-studio-code" "wireshark" "podman-desktop")
 function add_brew_install() {
   brew_to_install+=("$1")
 }
--- env/.env/env/rust.sh
+++ env/.env/env/rust.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 export RUST_BACKTRACE=1
 
-# setting PATH as the before calling .cargo/env is significant.
-export PATH="$HOME/.cargo/bin:$PATH"
-# cargo wants us to do this. really, it should no op in its current form, but
-# we call it for safety anyway :)
-. "$HOME/.cargo/env"
+if [ -f $HOME/.cargo/bin ]; then
+  # setting PATH as the before calling .cargo/env is significant.
+  export PATH="$HOME/.cargo/bin:$PATH"
+  # cargo wants us to do this. really, it should no op in its current form, but
+  # we call it for safety anyway :)
+  . "$HOME/.cargo/env"
 
-function rustify() {
-  if [ -f "$HOME/.cargo/env" ]; then
-    . $HOME/.cargo/env
-    # https://github.com/rust-unofficial/awesome-rust#productivity
-    global_crates=("zoxide" "bat" "bandwhich" "cargo-edit" "click") #  "dust"
+  function rustify() {
+    if [ -f "$HOME/.cargo/env" ]; then
+      . $HOME/.cargo/env
+      # https://github.com/rust-unofficial/awesome-rust#productivity
+      global_crates=("zoxide" "bat" "bandwhich" "cargo-edit" "click") #  "dust"
 
-    function upsertGlobalCrate() {
-      if [ ! $(command -v $1) ]; then
-        echo "cargo installing: $1"
-        cargo install "$1" -f
-      fi
-    }
-    for crate in "${global_crates[@]}"; do
-      # echo upserting crate $crate
-      upsertGlobalCrate $crate
-    done
-  fi
+      function upsertGlobalCrate() {
+        if [ ! $(command -v $1) ]; then
+          echo "cargo installing: $1"
+          cargo install "$1" -f
+        fi
+      }
+      for crate in "${global_crates[@]}"; do
+        # echo upserting crate $crate
+        upsertGlobalCrate $crate
+      done
+    fi
+  }
 
-}
-
-alias clippyfix="cargo clippy --fix --allow-staged --allow-dirty"
+  alias clippyfix="cargo clippy --fix --allow-staged --allow-dirty"
+fi
