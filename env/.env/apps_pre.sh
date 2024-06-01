84dccef17907731ab9a0bc2ace528c1ec957bae4
cdaringe <cdaringe@gmail.com>
2024-06-01 11:56:55 -0700

    feat(dotfiles): enhancing the radness

--- env/.env/apps_pre.sh
+++ env/.env/apps_pre.sh
@@ -5,8 +5,9 @@ export PATH="/opt/homebrew/bin/:$PATH"
 
 # See apps_post.js for syntax
 brew_to_install=("bash" "fd" "hub" "tree" "jq" "mosh" "ffmpeg")
+
 # openlens - manually install @alebcay/openlens-node-pod-menu, (optional, maybe) @andrea-falco/lens-multi-pod-logs
-brew_to_install_cask=("bluesnooze" "obs" "flux" "flycut" "rectangle" "iterm2" "visual-studio-code" "wireshark" "podman-desktop" "openlens")
+brew_to_install_cask=("bluesnooze" "obs" "flux" "flycut" "rectangle" "iterm2" "visual-studio-code" "wireshark" "openlens")
 function add_brew_install() {
   brew_to_install+=("$1")
 }
--- env/.env/env/docker.sh
+++ env/.env/env/docker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 export DOCKER_CLI_EXPERIMENTAL=enabled
-if [ ! -f /usr/local/bin/docker ]; then
+
+
+if command_exists podman; then
   alias docker="podman";
 fi
+
 function dkilla () {
   printf "\n==== Docker: Container Purge ====\n"
   printf "Purging $(docker ps -aq | wc -w | xargs) containers\n"
