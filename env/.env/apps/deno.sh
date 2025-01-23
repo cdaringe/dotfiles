cdd181e937788a728e0f84f022645141e8350bb3
cdaringe <cdaringe@gmail.com>
2025-01-22 20:08:41 -0800

    feat(dotfiles): enhancing the radness

--- env/.env/apps/deno.sh
+++ env/.env/apps/deno.sh
@@ -8,13 +8,13 @@ if [ -f "$DENO_ENV_FILE" ]; then
 fi
 
 deno_completions_filename=$HOME/.local/share/bash-completion/completions/deno.bash
-if command_exists deno; then
-  if [ ! $deno_completions_filename ]; then
-    mkdir -p "$(dirname $deno_completions_filename)"
-    deno completions bash > $deno_completions_filename
-  fi
-  source "$deno_completions_filename"
-fi
+# if command_exists deno; then
+#   if [ ! $deno_completions_filename ]; then
+#     mkdir -p "$(dirname $deno_completions_filename)"
+#     deno completions bash > $deno_completions_filename
+#   fi
+#   source "$deno_completions_filename"
+# fi
 
 function install_rad_next () {
   deno install --unstable -A -n rad https://raw.githubusercontent.com/cdaringe/rad/next/src/bin.ts
