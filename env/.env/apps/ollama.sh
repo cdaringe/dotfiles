09336bcadd3b8622ab9a65fd3dd14b1ad358f602
cdaringe <cdaringe@gmail.com>
2025-02-23 11:00:26 -0800

    feat(dotfiles): enhancing the radness

---
+++ env/.env/apps/ollama.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+function ollama_ui() {
+  docker run -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+}
