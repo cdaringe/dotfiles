ed93668ab22c68f6596dbe875125b2af2111c29d
cdaringe <cdaringe@gmail.com>
2025-03-02 16:26:49 -0800

    feat(dotfiles): enhancing the radness

--- env/.env/apps/ollama.sh
+++ env/.env/apps/ollama.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 function ollama_ui() {
-  echo "un/pw: admin@admin.com / admin"
-  docker run -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+  local OLLAMA_UI_IMAGE="ghcr.io/open-webui/open-webui:main"
+  docker pull "$OLLAMA_UI_IMAGE"
+  printf "\n\n\nun/pw: admin@admin.com / admin\n\n\n"
+  docker run -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always "$OLLAMA_UI_IMAGE"
 }
