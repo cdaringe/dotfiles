7f0b4b542b2fd6ba1c57cc1d2fbee16bf876612c
cdaringe <christopher.dieringer@gmail.com>
2026-03-29 13:08:48 -0700

    feat(dotfiles): enhancing the radness

--- env/.env/apps/ollama.sh
+++ env/.env/apps/ollama.sh
@@ -5,5 +5,5 @@ function ollama_ui() {
   docker pull "$OLLAMA_UI_IMAGE"
   printf "\n\n\nun/pw: admin@admin.com / admin\n\n\n"
   # docker run -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always "$OLLAMA_UI_IMAGE"
-  docker run -p 3000:8080 --network host -v open-webui:/app/backend/data --name open-webui --restart always "$OLLAMA_UI_IMAGE"
+  docker run --rm -p 3000:8080 --network host -v ~/src/open-webui/data:/app/backend/data --name open-webui  "$OLLAMA_UI_IMAGE"
 }
