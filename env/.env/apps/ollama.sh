ebfd3c585589a31d51e4004d395bab23c8079376
cdaringe <christopher.dieringer@gmail.com>
2026-03-29 13:33:23 -0700

    feat(dotfiles): enhancing the radness

--- env/.env/apps/ollama.sh
+++ env/.env/apps/ollama.sh
@@ -7,3 +7,14 @@ function ollama_ui() {
   # docker run -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always "$OLLAMA_UI_IMAGE"
   docker run --rm -p 3000:8080 --network host -v ~/src/open-webui/data:/app/backend/data --name open-webui  "$OLLAMA_UI_IMAGE"
 }
+
+function ollamasafe() {
+  GGML_NUM_THREADS=1 \
+    OLLAMA_CONTEXT_LENGTH=2048 \
+    OLLAMA_DEBUG=1 \
+    OLLAMA_FLASH_ATTENTION=0 \
+    OLLAMA_MAX_LOADED_MODELS=1 \
+    OLLAMA_NUM_PARALLEL=1 \
+    OLLAMA_HOST=0.0.0.0 \
+  ollama serve "$@"
+}
