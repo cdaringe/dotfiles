d1ddbc094815be061fb6f20e9a65b55f14ffacdd
cdaringe <cdaringe@gmail.com>
2026-05-18 16:30:04 -0700

    feat(dotfiles): enhancing the radness

--- env/.env/apps/node.sh
+++ env/.env/apps/node.sh
@@ -2,12 +2,6 @@
 # shellcheck shell=bash
 add_brew_install "fnm"
 
-FNM_PATH="$HOME/.fnm/fnm"
-if [ -d "$FNM_PATH" ]; then
-  export PATH="$FNM_PATH:$PATH"
-  eval "$(fnm env)"
-fi
-
 load_nvmrc() {
   if command_exists fnm && [[ -f .nvmrc && -r .nvmrc ]]; then
     fnm use
@@ -27,20 +21,13 @@ function npxbrk() {
 }
 
 ## npm
-alias nga="mv .npmrc .npmrcbu" # npmrc... go away!
-alias ncb="mv .npmrcbu .npmrc" # npmrc... come back!
 alias pkg="jq . package.json"
 function npxd() {
   node --inspect-brk "./node_modules/.bin/$1" "${@:2}"
 }
-
-alias ybs="yarn bootstrap"
 
 function node_dep_graph() {
   npx madge --image dependency-graph.png "$1"
 }
 
 alias p="pnpm"
-alias pi="pnpm install"
-alias x="pnpm exec"
-#alias pd="pnpm dlx"
